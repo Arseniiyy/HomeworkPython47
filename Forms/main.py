ef2325733ec24ac3c573ab4e7f7b22ccf605e24c
Арсений <Penkovskiy2024@mail.ru>
2025-11-12 18:13:24 +0300

    Add files via upload

---
+++ Forms/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI,Form
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return FileResponse("public/index.html")
+
+@app.post("/postdata")
+def postdata(username = Form(),userage = Form()):
+    return{"name":username,"userage":userage}
